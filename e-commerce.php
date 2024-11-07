1f848b130ccaa9d11f716f71c2aa271e0fb7326c
ThibaultF <thibault.frescaline@ynov.com>
2024-11-07 14:53:55 +0100

    finished

--- e-commerce.php
+++ e-commerce.php
@@ -25,6 +25,36 @@ try {
 // faker instance
 $faker = Faker\Factory::create();
 
+// initiate the status tables
+$stmt = $pdo->prepare('SELECT COUNT(*) FROM command_status');
+$stmt->execute();
+$command_status_count = $stmt->fetchColumn();
+if ($command_status_count === 0) {
+    $stmt = $pdo->prepare('INSERT INTO command_status (command_status) VALUES (?)');
+    $stmt->execute(['pending']);
+    $stmt->execute(['shipped']);
+    $stmt->execute(['delivered']);
+}
+
+$stmt = $pdo->prepare('SELECT COUNT(*) FROM cart_status');
+$stmt->execute();
+$cart_status_count = $stmt->fetchColumn();
+if ($cart_status_count === 0) {
+    $stmt = $pdo->prepare('INSERT INTO cart_status (cart_status) VALUES (?)');
+    $stmt->execute(['empty']);
+    $stmt->execute(['not_empty']);
+    $stmt->execute(['deleted']);
+}
+
+$stmt = $pdo->prepare('SELECT COUNT(*) FROM payment_type');
+$stmt->execute();
+$payment_type_count = $stmt->fetchColumn();
+if ($payment_type_count === 0) {
+    $stmt = $pdo->prepare('INSERT INTO payment_type (type_name) VALUES (?)');
+    $stmt->execute(['credit_card']);
+    $stmt->execute(['paypal']);
+}
+
 // loop to generate the general data
 for ($i = 0; $i < 100; $i++) {
     // user data
@@ -68,6 +98,7 @@ function insertProductRelation($pdo, $relation_cart_id, $relation_command_id, $p
     $stmt->execute([$relation_cart_id, $relation_command_id, $id_product, $quantity]);
 }
 
+// loop for the command and cart data
 for ($i = 0; $i < 100; $i++){
     // command data
     $stmt = $pdo->prepare('SELECT COUNT(*) FROM user');
@@ -167,6 +198,81 @@ for ($i = 0; $i < 100; $i++){
     }
     $stmt = $pdo->prepare('UPDATE cart SET total_price = ? WHERE id_cart = ?');
     $stmt->execute([$cart_total_price, $i + 1]);
+}
+
+//payment data
+$stmt = $pdo->prepare('SELECT COUNT(*) FROM user');
+$stmt->execute();
+$user_count = $stmt->fetchColumn();
+for ($i = 0; $i < $user_count; $i++) {
+    $payment_user_id = $i + 1;
+    $payment_type = $faker->numberBetween(1, 2);
+    $payment_details = hash('sha256', $faker->creditCardNumber() . $faker->creditCardExpirationDateString());
+    $stmt = $pdo->prepare('INSERT INTO payment (id_user, id_payment_type, payment_details) VALUES (?, ?, ?)');
+    $stmt->execute([$payment_user_id, $payment_type, $payment_details]);
+}
+
+//invoice data
+$stmt = $pdo->prepare('SELECT COUNT(*) FROM command');
+$stmt->execute();
+$command_count = $stmt->fetchColumn();
+$stmt = $pdo->prepare('SELECT COUNT(*) FROM payment');
+$stmt->execute();
+$payment_count = $stmt->fetchColumn();
+
+for ($i = 0; $i < $command_count; $i++) {
+    $invoice_command_id = $i + 1;
+    $stmt = $pdo->prepare('SELECT id_user, total_price FROM command WHERE id_command = ?');
+    $stmt->execute([$invoice_command_id]);
+    $invoice_command = $stmt->fetchAll();
+    foreach ($invoice_command as $command) {
+        $invoice_user_id = $command['id_user'];
+        $invoice_total_price = $command['total_price'];
+    }
+
+    $stmt = $pdo->prepare('SELECT id_payment FROM payment WHERE id_user = ?');
+    $stmt->execute([$invoice_user_id]);
+    $invoice_payment_id = $stmt->fetchColumn();
+
+    $stmt = $pdo->prepare('INSERT INTO invoice (id_command, id_payment, total_price) VALUES (?, ?, ?)');
+    $stmt->execute([$invoice_command_id, $invoice_payment_id, $invoice_total_price]);
+}
+
+//rate data
+$stmt = $pdo->prepare('SELECT COUNT(*) FROM product');
+$stmt->execute();
+$product_count = $stmt->fetchColumn();
+$stmt = $pdo->prepare('SELECT COUNT(*) FROM user');
+$stmt->execute();
+$user_count = $stmt->fetchColumn();
+
+for ($i = 0; $i < 100; $i++) {
+    $rate_product_id = $faker->numberBetween(1, $product_count);
+    $rate_user_id = $faker->numberBetween(1, $user_count);
+    $rating = $faker->numberBetween(1, 5);
+    $rate_comment = $faker->sentence();
+    $stmt = $pdo->prepare('INSERT INTO rate (id_product, id_user, rate, comment) VALUES (?, ?, ?, ?)');
+    $stmt->execute([$rate_product_id, $rate_user_id, $rating, $rate_comment]);
+}
+
+//photo data
+//products photo
+for ($i = 0; $i < $product_count; $i++) {
+    $stmt = $pdo->prepare('SELECT product_name FROM product WHERE id_product = ?');
+    $stmt->execute([$i + 1]);
+    $product_name = $stmt->fetchColumn();
+    $photo_url = $faker->imageUrl(640, 480, "Product", true, $product_name);
+    $stmt = $pdo->prepare('INSERT INTO photo (url, id_product) VALUES (?, ?)');
+    $stmt->execute([$photo_url, $i + 1]);
+}
+//user photo
+for ($i = 0; $i < $user_count; $i++) {
+    $stmt = $pdo->prepare('SELECT id_user FROM user WHERE id_user = ?');
+    $stmt->execute([$i + 1]);
+    $user_name = $stmt->fetchColumn();
+    $photo_url = $faker->imageUrl(640, 480, "User", true, $user_name);
+    $stmt = $pdo->prepare('INSERT INTO photo (url, id_user) VALUES (?, ?)');
+    $stmt->execute([$photo_url, $i + 1]);
 }
 
 echo "success";
